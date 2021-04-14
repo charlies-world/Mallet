763ac41ca35d944cf2b496e23735ddf6fc246d67
charlies-world <charliew3169@gmail.com>
2021-04-13 21:16:29 -0400

    Began working on version 2 of the composition.

--- Workspace.py
+++ Workspace.py
@@ -75,11 +75,11 @@ class Mallet:
     
     def is_not_at_minimum_number_of_notes(self):
         number_of_notes = self.get_number_of_notes()
-        return (number_of_notes >= self.minimum_number_of_notes_per_measure)    
+        return (number_of_notes > self.minimum_number_of_notes_per_measure)    
     
     def is_not_at_max_number_of_notes(self):
         number_of_notes = self.get_number_of_notes()
-        return (number_of_notes <= self.maximum_number_of_notes_per_measure)
+        return (number_of_notes < self.maximum_number_of_notes_per_measure)
     
     def get_number_of_notes(self):
         number_of_notes = 0
@@ -103,41 +103,105 @@ class Mallet:
             measure.add_beat(beat)
         self.voice.add_measure(measure)
         return
+    
+class Section:
+    def __init__(self, letter):
+        self.letter = letter
+        return
+    
+    def get_letter(self):
+        return self.letter
+    
+    def set_composition(self, composition):
+        self.composition = composition
+        return
+    
+    def get_composition(self):
+        return self.composition
+    
+def version_one():
+    pitch_set = [
+                ['G#', 2],
+                ['B', 2],
+                ['D', 3],
+                ['F#', 3],
+                ['A', 4],
+                ['C#', 4],
+                ['A', 5],
+                ['E', 5]
+                ]
+    number_of_measures = 300
+    number_of_beats_per_measure = 8
+    maximum_number_of_notes_per_measure = 5
+    minimum_number_of_notes_per_measure = 1
+    rhythmic_value_for_beat = 'eighth'
+    quarter_note_tempo = 1000
+    likelihood_of_change = 1000
 
-pitch_set = [
-            ['G#', 2],
-            ['B', 2],
-            ['D', 3],
-            ['F#', 3],
-            ['A', 4],
-            ['C#', 4],
-            ['A', 5],
-            ['E', 5]
-            ]
-number_of_measures = 300
-number_of_beats_per_measure = 8
-maximum_number_of_notes_per_measure = 5
-minimum_number_of_notes_per_measure = 1
-rhythmic_value_for_beat = 'eighth'
-quarter_note_tempo = 1000
-likelihood_of_change = 1000
+    c = Composition()
+    c.set_quarter_note_bpm(quarter_note_tempo)
 
+    for i in range(0, len(pitch_set)):
+        mallet_voice = Mallet(pitch_set[i],
+                    number_of_beats_per_measure,
+                    rhythmic_value_for_beat,
+                    likelihood_of_change,
+                    number_of_measures,
+                    maximum_number_of_notes_per_measure,
+                    minimum_number_of_notes_per_measure,
+                    i)
+        voice = mallet_voice.compose()
+        c.add_voice(voice)
 
-c = Composition()
-c.set_quarter_note_bpm(quarter_note_tempo)
-
-for i in range(0, len(pitch_set)):
-    mallet_voice = Mallet(pitch_set[i],
-                   number_of_beats_per_measure,
-                   rhythmic_value_for_beat,
-                   likelihood_of_change,
-                   number_of_measures,
-                   maximum_number_of_notes_per_measure,
-                   minimum_number_of_notes_per_measure,
-                   i)
-    voice = mallet_voice.compose()
-    c.add_voice(voice)
-
-now = datetime.now()
-current_time_formatted = now.strftime("%d.%m.%Y %H.%M.%S")
-c.write_midi_data('mallet' + ' ' + current_time_formatted)
+    now = datetime.now()
+    current_time_formatted = now.strftime("%d.%m.%Y %H.%M.%S")
+    c.write_midi_data('mallet' + ' ' + current_time_formatted)
+    
+def version_two():
+    pitch_set = [
+                ['G#', 2],
+                ['B', 2],
+                ['D', 3],
+                ['F#', 3],
+                ['A', 4],
+                ['C#', 4],
+                ['A', 5],
+                ['E', 5]
+                ]
+    number_of_measures = 16
+    number_of_beats_per_measure = 8
+    maximum_number_of_notes_per_measure = 5
+    minimum_number_of_notes_per_measure = 1
+    rhythmic_value_for_beat = 'eighth'
+    quarter_note_tempo = 120
+    likelihood_of_change = 64
+    form = 'ABAB'    
+    
+    form_sections = []
+    for i in range(0, len(form)):
+        if not form[i] in form_sections:
+            section = Section(form[i])
+            form_sections.append(section)
+    
+    for j in range(0, len(form_sections)):
+        c = Composition()
+        c.set_quarter_note_bpm(quarter_note_tempo)
+        for m in range(0, len(pitch_set)):
+            mallet_voice = Mallet(pitch_set[m],
+                number_of_beats_per_measure,
+                rhythmic_value_for_beat,
+                likelihood_of_change,
+                number_of_measures,
+                maximum_number_of_notes_per_measure,
+                minimum_number_of_notes_per_measure,
+                i)
+            voice = mallet_voice.compose()
+            c.add_voice(voice)
+        form_sections[j].set_composition(c)
+        
+    for n in range(0, len(form_sections)):
+        now = datetime.now()
+        current_time_formatted = now.strftime("%d.%m.%Y %H.%M.%S")
+        form_sections[n].get_composition().write_midi_data('mallet' + ' ' + current_time_formatted + ' ' + form_sections[n].get_letter())
+    
+version_two()
